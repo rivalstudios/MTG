d2086186e13401ee0708a81976472e417944aa4f
rivalstudios <mparinas@gmail.com>
2011-06-29 12:49:03 -0700

    Added javadoc comments to Constants.java.

--- src/ca/rivalstudios/mtg/Constants.java
+++ src/ca/rivalstudios/mtg/Constants.java
@@ -1,28 +1,52 @@
 package ca.rivalstudios.mtg;
 
+/**
+ * Stores all constants used by the extension
+ *
+ * @author Melvin Parinas
+ * @version 1.0
+ */
 public class Constants {
-	
+	/*
+	 * The current version of the server extension.
+	 */
 	public final static String SERVER_VERSION = "1.0";
 	
-	public final static String X = "px";
-	public final static String Y = "py";
+	/*
+	 * Reference to an object's position.
+	 */
+	public final static String X = "x";
+	public final static String Y = "y";
 
 	public static int ERROR = -1;
 	public static int OK = 1;
-	
+
+	/*
+	 * Player team codes.
+	 */
 	public static int TEAM_RED = 1;
 	public static int TEAM_BLUE = 2;
-	
+
+	/*
+	 * Experience points values for all characters
+	 */
 	public static int XP_TOWER = 100;
 	public static int XP_MINION_LEVEL1 = 5;
 	public static int XP_MINION_LEVEL2 = 8;
+	public static int XP_PLAYER = 15;
 
+	/*
+	 * Collider radiuses for all characters.
+	 */
 	public static int RADIUS_SHELL = 1;
 	public static int RADIUS_MINION = 5;
 	public static int RADIUS_PLAYER = 10;
 	public static int RADIUS_TOWER = 15;
 	public static int RADIUS_THRONE = 20;
-	
+
+	/*
+	 * Map size reference values.
+	 */
 	public static int WORLD_MIN_X = -30;
 	public static int WORLD_MAX_X = 30;
 	public static int WORLD_MIN_Z = -30;
